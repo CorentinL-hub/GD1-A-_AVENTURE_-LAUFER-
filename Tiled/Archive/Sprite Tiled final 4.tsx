f4ab32128493394ddb021f6245ad66e640a3e23c
Corentin-L <corentin.laufer@laposte.net>
2021-05-09 17:24:10 +0200

    dgftd
    erte

---
+++ Tiled/Archive/Sprite Tiled final 4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Sprite Tiled final 4" tilewidth="32" tileheight="32" tilecount="144" columns="12">
+ <image source="Asset/Sprite Tiled final 4.png" width="400" height="400"/>
+</tileset>
---
+++ Tiled/Sprite Tiledfinal.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Sprite Tiledaezaxxaaaa" tilewidth="32" tileheight="32" tilecount="144" columns="12">
+ <image source="Asset/Sprite Tiledaezaxxaaaa.png" width="400" height="400"/>
+</tileset>
